64e66770b68c9768aa87049febf49fc255a4adc9
jerry <jerryyangshaoern@gmail.com>
2024-10-07 20:34:59 +0800

    Update chatbot class

---
+++ AY 24-25 Sem 1/Introduction to Natural Language Processing/chatbot.py
@@ -0,0 +1,23 @@
+
+
+class chatbot():
+    
+    def __init__(self):
+        self.client = Groq()
+    
+    def extract_response(self, response):
+        pass
+        
+    def fetch_response(self, message):
+        # use chat completion function and insert this here
+        pass
+        
+    
+        
+        
+        
+        
+        
+        
+        
+        
